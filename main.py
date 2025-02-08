26200167ac51d7014edc4e68f8fde90901353936
Nguyễn Phan Vũ <npvu1510@gmail.com>
2025-02-08 17:06:29 +0700

    run 08-02-2025

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ def main():
 
     # BUY
     # allInOnePlayer(RESET_TIME['Suarez'], grade= 5, priceType = PRICE_TYPES['100'])
-    allInOnePlayer(False, grade= 8, priceType = PRICE_TYPES['1000'])
+    # allInOnePlayer(False, grade=8, priceType = PRICE_TYPES['100'], autoCancel=False, quantity=3)
 
 
-    # players = [{'row': 5, 'resetTime': RESET_TIME['Fekir'], 'quantity': 3,'priceType': PRICE_TYPES['100']}, {'row': 12, 'resetTime': RESET_TIME['Banega'], 'quantity': 3,'priceType': PRICE_TYPES['100']}, {'row': 4, 'resetTime': RESET_TIME['Suarez'],'quantity': 1,'priceType': PRICE_TYPES['100']}]
+    players = [{'row': 1, 'resetTime': RESET_TIME['Oyarzabal'], 'quantity': 2,'priceType': PRICE_TYPES['100']}
+               , {'row': 2, 'resetTime': RESET_TIME['Illarramendi'], 'quantity': 2,'priceType': PRICE_TYPES['100']}
+               , {'row':8, 'resetTime': RESET_TIME['Matheus Nunes'], 'quantity': 2,'priceType': PRICE_TYPES['100']}
+               , {'row': 6, 'resetTime': RESET_TIME['Abedi'], 'quantity': 2,'priceType': PRICE_TYPES['100']}]
 
     # players = [{'row': 6, 'resetTime': RESET_TIME['Fekir'], 'quantity': 2,'priceType': PRICE_TYPES['100']}, {'row': 5, 'resetTime': RESET_TIME['Danjuma'], 'quantity': 2,'priceType': PRICE_TYPES['100']}, {'row': 7, 'resetTime': RESET_TIME['Inaki'],'quantity': 1,'priceType': PRICE_TYPES['100']}, {'row': 3, 'resetTime': RESET_TIME['Suarez'],'quantity': 1,'priceType': PRICE_TYPES['100']}, {'row': 8, 'resetTime': RESET_TIME['Abedi'],'quantity': 1,'priceType': PRICE_TYPES['100']}]
     
-    
     # players = [{'row': 8, 'resetTime': RESET_TIME['Kewell'], 'quantity': 2,'priceType': PRICE_TYPES['100']}, {'row': 14, 'resetTime': RESET_TIME['Cristiano Ronaldo'], 'quantity': 1,'priceType': PRICE_TYPES['1000']},]
-    # buyMultiPlayers(players, autoDelay=True)
+    buyMultiPlayers(players, autoDelay=False)
 
 
     # SELL
